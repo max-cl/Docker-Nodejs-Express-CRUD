3583b26814c193f66c51c6080b77d7af5e8b18db
max <maximiliano.arias@gmail.com>
2020-10-01 11:11:34 +0200

    #

--- node_backend/validator/schemas/todo.schema.js
+++ node_backend/validator/schemas/todo.schema.js
@@ -3,24 +3,24 @@ const constants = require('./constants.schema');
 
 const schemas = { 
 	get_todo: Joi.object().keys({ 
-        id_user: constants.id_user_validation
+                id_user: constants.id_user_validation
 	}),
 
 	create_todo: Joi.object().keys({ 
-        id_user: constants.id_user_validation,
-        title: constants.title_validation
+                id_user: constants.id_user_validation,
+                title: constants.title_validation
 	}),
 
 	update_todo: Joi.object().keys({ 
-        id_user: constants.id_user_validation,
-        title: constants.title_validation,
-        id: constants.id_validation,
-        done: constants.done_validation
+                id_user: constants.id_user_validation,
+                title: constants.title_validation,
+                id: constants.id_validation,
+                done: constants.done_validation
 	}),
 
 	delete_todo: Joi.object().keys({ 
-        id_user: constants.id_user_validation,
-        id: constants.id_validation
+                id_user: constants.id_user_validation,
+                id: constants.id_validation
 	})
 }; 
 
